7d6d3743963ba6e621055be98cc2af2bc09bf336
admin <admin@>
2017-03-20 16:15:04 +0800

    viewController内容提交

--- ZXXCarousel/ZXXCarousel/ViewController.swift
+++ ZXXCarousel/ZXXCarousel/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
+    let carouselView = CarouselView(Y: 64, H: 200)
+
+    var modelArr = [CarouselModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
+        getArrayFromWeb()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +29,23 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController {
+    func setupUI() {
+        self.view.addSubview(carouselView)
+    }
+    
+    func getArrayFromWeb() {
+        let manager = AFHTTPSessionManager()
+        manager.get("http://www.douyutv.com/api/v1/slide/6", parameters: ["version" : "2.300"], progress: nil, success: { (task:URLSessionDataTask, json:Any) in
+//            print("jsonData: \(json)")
+            guard let dataDic = json as? [String : NSObject] else { return }
+            guard let dataArr = dataDic["data"] as? [[String : NSObject]] else { return }
+            for dic in dataArr {
+                self.modelArr.append(CarouselModel(dic: dic))
+            }
+            self.carouselView.carouselModelArr = self.modelArr
+        }) { (task:URLSessionDataTask?, error:Error) in
+            print("error : \(error)")
+        }
+    }
+}
